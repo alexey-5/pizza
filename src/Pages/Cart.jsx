bc6a8063a6bd3b2f83731ccd6904354ebfe44162
alexey-5 <107930630+alexey-5@users.noreply.github.com>
2022-11-28 20:29:58 +0300

    адаптив корзины

--- src/Pages/Cart.jsx
+++ src/Pages/Cart.jsx
@@ -10,11 +10,11 @@ function Cart() {
   const sum = useSelector((state) => state.cart.sum);
   const dispatch = useDispatch();
 
-  if (countTotal === 0) return <CartEmpty></CartEmpty>;//условный рендеринг
+  if (countTotal === 0) return <CartEmpty></CartEmpty>; //условный рендеринг
 
   return (
     <div className="container container--cart">
-            <div className="cart">
+      <div className="cart">
         <div className="cart__top">
           <h2 className="content__title">
             <svg
@@ -86,89 +86,111 @@ function Cart() {
               />
             </svg>
 
-            <span onClick={()=>{dispatch(deleteAll())}}>Очистить корзину</span>
+            <span
+              onClick={() => {
+                dispatch(deleteAll());
+              }}
+            >
+              Очистить корзину
+            </span>
           </div>
         </div>
         <div className="content__items">
-          {pizzaItem.map((elem)=>
+          {pizzaItem.map((elem) => (
             <div key={elem.id} className="cart__item">
-            <div className="cart__item-img">
-              <img
-                className="pizza-block__image"
-                src={elem.imageUrl}
-                alt="Pizza"
-              />
-            </div>
-            <div className="cart__item-info">
-              <h3>{elem.name}</h3>
-              <p>
-                {elem.types?"традиционное":"тонкое"}
-                , {elem.sizes} см.</p>
-            </div>
-            <div className="cart__item-count">
-              <div onClick={()=>dispatch(minus(elem.id))} className="button button--outline button--circle cart__item-count-minus">
-                <svg
-                  width="10"
-                  height="10"
-                  viewBox="0 0 10 10"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    d="M5.92001 3.84V5.76V8.64C5.92001 9.17016 5.49017 9.6 4.96001 9.6C4.42985 9.6 4.00001 9.17016 4.00001 8.64L4 5.76L4.00001 3.84V0.96C4.00001 0.42984 4.42985 0 4.96001 0C5.49017 0 5.92001 0.42984 5.92001 0.96V3.84Z"
-                    fill="#EB5A1E"
+              <div  className="cart__item-pizza">
+                <div className="cart__item-img">
+                  <img
+                    className="pizza-block__image"
+                    src={elem.imageUrl}
+                    alt="Pizza"
                   />
-                  <path
-                    d="M5.75998 5.92001L3.83998 5.92001L0.959977 5.92001C0.429817 5.92001 -2.29533e-05 5.49017 -2.29301e-05 4.96001C-2.2907e-05 4.42985 0.429817 4.00001 0.959977 4.00001L3.83998 4L5.75998 4.00001L8.63998 4.00001C9.17014 4.00001 9.59998 4.42985 9.59998 4.96001C9.59998 5.49017 9.17014 5.92001 8.63998 5.92001L5.75998 5.92001Z"
-                    fill="#EB5A1E"
-                  />
-                </svg>
+                </div>
+                <div className="cart__item-info">
+                  <h3>{elem.name}</h3>
+                  <p>
+                    {elem.types ? "традиционное" : "тонкое"}, {elem.sizes} см.
+                  </p>
+                </div>
               </div>
-              <b>{elem.count} </b>
-              <div onClick={()=>dispatch(plus(elem.id))} className="button button--outline button--circle cart__item-count-plus">
-                <svg
-                  width="10"
-                  height="10"
-                  viewBox="0 0 10 10"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    d="M5.92001 3.84V5.76V8.64C5.92001 9.17016 5.49017 9.6 4.96001 9.6C4.42985 9.6 4.00001 9.17016 4.00001 8.64L4 5.76L4.00001 3.84V0.96C4.00001 0.42984 4.42985 0 4.96001 0C5.49017 0 5.92001 0.42984 5.92001 0.96V3.84Z"
-                    fill="#EB5A1E"
-                  />
-                  <path
-                    d="M5.75998 5.92001L3.83998 5.92001L0.959977 5.92001C0.429817 5.92001 -2.29533e-05 5.49017 -2.29301e-05 4.96001C-2.2907e-05 4.42985 0.429817 4.00001 0.959977 4.00001L3.83998 4L5.75998 4.00001L8.63998 4.00001C9.17014 4.00001 9.59998 4.42985 9.59998 4.96001C9.59998 5.49017 9.17014 5.92001 8.63998 5.92001L5.75998 5.92001Z"
-                    fill="#EB5A1E"
-                  />
-                </svg>
-              </div>
-            </div>
-            <div className="cart__item-price">
-              <b>{elem.price} ₽</b>
-            </div>
-            <div className="cart__item-remove">
-              <div onClick={()=>{dispatch(deleteItem(elem.id))}} className="button button--outline button--circle">
-                <svg
-                  width="10"
-                  height="10"
-                  viewBox="0 0 10 10"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    d="M5.92001 3.84V5.76V8.64C5.92001 9.17016 5.49017 9.6 4.96001 9.6C4.42985 9.6 4.00001 9.17016 4.00001 8.64L4 5.76L4.00001 3.84V0.96C4.00001 0.42984 4.42985 0 4.96001 0C5.49017 0 5.92001 0.42984 5.92001 0.96V3.84Z"
-                    fill="#EB5A1E"
-                  />
-                  <path
-                    d="M5.75998 5.92001L3.83998 5.92001L0.959977 5.92001C0.429817 5.92001 -2.29533e-05 5.49017 -2.29301e-05 4.96001C-2.2907e-05 4.42985 0.429817 4.00001 0.959977 4.00001L3.83998 4L5.75998 4.00001L8.63998 4.00001C9.17014 4.00001 9.59998 4.42985 9.59998 4.96001C9.59998 5.49017 9.17014 5.92001 8.63998 5.92001L5.75998 5.92001Z"
-                    fill="#EB5A1E"
-                  />
-                </svg>
-              </div>
-            </div>
-          </div>)}
 
+              <div className="cart__item-calc">
+                <div className="cart__item-count">
+                  <div
+                    onClick={() => dispatch(minus(elem.id))}
+                    className="button button--outline button--circle cart__item-count-minus"
+                  >
+                    <svg
+                      width="10"
+                      height="10"
+                      viewBox="0 0 10 10"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M5.92001 3.84V5.76V8.64C5.92001 9.17016 5.49017 9.6 4.96001 9.6C4.42985 9.6 4.00001 9.17016 4.00001 8.64L4 5.76L4.00001 3.84V0.96C4.00001 0.42984 4.42985 0 4.96001 0C5.49017 0 5.92001 0.42984 5.92001 0.96V3.84Z"
+                        fill="#EB5A1E"
+                      />
+                      <path
+                        d="M5.75998 5.92001L3.83998 5.92001L0.959977 5.92001C0.429817 5.92001 -2.29533e-05 5.49017 -2.29301e-05 4.96001C-2.2907e-05 4.42985 0.429817 4.00001 0.959977 4.00001L3.83998 4L5.75998 4.00001L8.63998 4.00001C9.17014 4.00001 9.59998 4.42985 9.59998 4.96001C9.59998 5.49017 9.17014 5.92001 8.63998 5.92001L5.75998 5.92001Z"
+                        fill="#EB5A1E"
+                      />
+                    </svg>
+                  </div>
+                  <b>{elem.count} </b>
+                  <div
+                    onClick={() => dispatch(plus(elem.id))}
+                    className="button button--outline button--circle cart__item-count-plus"
+                  >
+                    <svg
+                      width="10"
+                      height="10"
+                      viewBox="0 0 10 10"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M5.92001 3.84V5.76V8.64C5.92001 9.17016 5.49017 9.6 4.96001 9.6C4.42985 9.6 4.00001 9.17016 4.00001 8.64L4 5.76L4.00001 3.84V0.96C4.00001 0.42984 4.42985 0 4.96001 0C5.49017 0 5.92001 0.42984 5.92001 0.96V3.84Z"
+                        fill="#EB5A1E"
+                      />
+                      <path
+                        d="M5.75998 5.92001L3.83998 5.92001L0.959977 5.92001C0.429817 5.92001 -2.29533e-05 5.49017 -2.29301e-05 4.96001C-2.2907e-05 4.42985 0.429817 4.00001 0.959977 4.00001L3.83998 4L5.75998 4.00001L8.63998 4.00001C9.17014 4.00001 9.59998 4.42985 9.59998 4.96001C9.59998 5.49017 9.17014 5.92001 8.63998 5.92001L5.75998 5.92001Z"
+                        fill="#EB5A1E"
+                      />
+                    </svg>
+                  </div>
+                </div>
+                <div className="cart__item-price">
+                  <b>{elem.price} ₽</b>
+                </div>
+                <div className="cart__item-remove">
+                  <div
+                    onClick={() => {
+                      dispatch(deleteItem(elem.id));
+                    }}
+                    className="button button--outline button--circle"
+                  >
+                    <svg
+                      width="10"
+                      height="10"
+                      viewBox="0 0 10 10"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M5.92001 3.84V5.76V8.64C5.92001 9.17016 5.49017 9.6 4.96001 9.6C4.42985 9.6 4.00001 9.17016 4.00001 8.64L4 5.76L4.00001 3.84V0.96C4.00001 0.42984 4.42985 0 4.96001 0C5.49017 0 5.92001 0.42984 5.92001 0.96V3.84Z"
+                        fill="#EB5A1E"
+                      />
+                      <path
+                        d="M5.75998 5.92001L3.83998 5.92001L0.959977 5.92001C0.429817 5.92001 -2.29533e-05 5.49017 -2.29301e-05 4.96001C-2.2907e-05 4.42985 0.429817 4.00001 0.959977 4.00001L3.83998 4L5.75998 4.00001L8.63998 4.00001C9.17014 4.00001 9.59998 4.42985 9.59998 4.96001C9.59998 5.49017 9.17014 5.92001 8.63998 5.92001L5.75998 5.92001Z"
+                        fill="#EB5A1E"
+                      />
+                    </svg>
+                  </div>
+                </div>
+              </div>
+            </div>
+          ))}
         </div>
         <div className="cart__bottom">
           <div className="cart__bottom-details">
@@ -178,17 +200,15 @@ function Cart() {
             </span>
             <span>
               {" "}
-              Сумма заказа: <b>{sum } ₽</b>{" "}
+              Сумма заказа: <b>{sum} ₽</b>{" "}
             </span>
           </div>
           <div className="cart__bottom-buttons">
-            <Link to="/" className="button button--outline button--add go-back-btn">
-              <svg
-                width="8"
-                height="14"
-                viewBox="0 0 8 14"
-                fill="none"
-              >
+            <Link
+              to="/"
+              className="button button--outline button--add go-back-btn"
+            >
+              <svg width="8" height="14" viewBox="0 0 8 14" fill="none">
                 <path
                   d="M7 13L1 6.93015L6.86175 1"
                   stroke="#D3D3D3"
@@ -205,7 +225,7 @@ function Cart() {
             </div>
           </div>
         </div>
-      </div> 
+      </div>
     </div>
   );
 }
--- src/Pages/PizzaMore.jsx
+++ src/Pages/PizzaMore.jsx
@@ -30,7 +30,7 @@ const PizzaMore = () => {
   if (!pizza) return " Загрузка....";
   return (
     <div className="container">
-      <img src={pizza.imageUrl}></img>
+      <img className="pizzamore_img" src={pizza.imageUrl}></img>
       <h1 >Pizza</h1>
       <p>{pizza.name}</p>
       <h4>{pizza.price}</h4>
